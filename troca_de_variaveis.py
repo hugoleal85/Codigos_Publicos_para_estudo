e76b7f0ac6180e0678f21744061016dd3090695f
Hugo Leal <hugoleal85@gmail.com>
2023-04-26 22:32:53 -0300

    correção da docstring do arquivo

--- troca_de_variaveis.py
+++ troca_de_variaveis.py
@@ -17,8 +17,8 @@ print(f"O valor final da variável b é: {b}")
 
 '''Este código em Python demonstra duas maneiras diferentes de trocar os valores das variáveis a e b.
 
-A primeira maneira, usando uma variável auxiliar temp, consiste em armazenar o valor de a em temp, atribuir o valor de b a a e, em seguida, atribuir o valor de temp a b. Isso efetivamente troca os valores das variáveis a e b.
+A primeira maneira, usando uma variável auxiliar "temp", consiste em armazenar o valor de "a" em "temp", atribuir o valor de "b" a "a" e, em seguida, atribuir o valor de "temp" a "b". Isso efetivamente troca os valores das variáveis "a" e "b".
 
-A segunda maneira de trocar os valores das variáveis é através de atribuição múltipla. Nesse caso, os valores das variáveis a e b são trocados em uma única linha, sem a necessidade de uma variável auxiliar.
+A segunda maneira de trocar os valores das variáveis é através de atribuição múltipla. Nesse caso, os valores das variáveis "a" e "b" são trocados em uma única linha, sem a necessidade de uma variável auxiliar.
 
 A primeira maneira é mais útil em situações em que você não pode usar atribuição múltipla, como em linguagens de programação que não a suportam. A segunda maneira é mais conveniente e pode tornar o código mais legível em Python.'''
